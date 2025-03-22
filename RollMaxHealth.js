4b03908444ccfac9f0e7d6cb4c35e072e42cd1dd
BungFritz <tfritz2718@gmail.com>
2025-03-22 19:23:03 +1030

    Update RollMaxHealth.js

--- RollMaxHealth.js
+++ RollMaxHealth.js
@@ -6,8 +6,6 @@ for(const token of canvas.tokens.placeables){
     if(actor.system.attributes.hp.formula !== undefined && actor.type == 'npc'){
         let val = await new Roll(actor.system.attributes.hp.formula).evaluate()
         for(actor of token.document.actors){
-            console.log(actor)
-            console.log(val.total)
             actor.update({
                 "system.attributes.hp.value": val.total,
                 "system.attributes.hp.max": val.total,
