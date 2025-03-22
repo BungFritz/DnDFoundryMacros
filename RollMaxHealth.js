fa7214788172fc4c0f25fb2fd04102a1b41247d7
BungFritz <tfritz2718@gmail.com>
2025-03-22 19:23:13 +1030

    Update RollMaxHealth.js

--- RollMaxHealth.js
+++ RollMaxHealth.js
@@ -1,8 +1,6 @@
 // Roll the max health of all npc tokens in the scene.
 for(const token of canvas.tokens.placeables){
     let actor = game.actors.get(token.document.actorId)
-    console.log(token)
-
     if(actor.system.attributes.hp.formula !== undefined && actor.type == 'npc'){
         let val = await new Roll(actor.system.attributes.hp.formula).evaluate()
         for(actor of token.document.actors){
